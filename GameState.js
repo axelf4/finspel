81e799c44814e47eb0c95565738dc74f049be804
Axel Forsman <axelsfor@gmail.com>
2016-01-16 14:24:50 +0100

    Fixed when the mothership spawns.

--- GameState.js
+++ GameState.js
@@ -346,8 +346,7 @@ define(["three", "fowl", "GPUParticleSystem", "game", "components", "MainMenuSta
 				}
 
 				var mothershipExists = updateMothership(dt);
-				// if (!mothershipExists && !this.hadMothership && this.score > 25) {
-				if (!mothershipExists && !this.hadMothership && this.score > 5) {
+				if (!mothershipExists && !this.hadMothership && this.score > 25) {
 					this.hadMothership = true;
 				   	spawnMothership();
 				}
